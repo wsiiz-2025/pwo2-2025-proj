32442b99c825f91457fa52664f2daf5ee7187097
Maciej Barć <xgqt@gentoo.org>
2025-07-06 11:07:56 +0200

    feat(tf/install_packages.sh): rewrite to install k3s
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- tf/install_packages.sh
+++ tf/install_packages.sh
@@ -1,62 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-install_debian() {
-    export DEBIAN_FRONTEND=noninteractive
-    apt-get update && apt-get upgrade -y
-    apt-get install -y ca-certificates curl
+trap "exit 128" INT
+set -eux
 
-    install -m 0755 -d /etc/apt/keyrings
+export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
+export K3S_KUBECONFIG_MODE="644"
+export INSTALL_K3S_EXEC="--disable=metrics-server --node-name=aws --write-kubeconfig-mode=${K3S_KUBECONFIG_MODE}"
 
-    curl -fsSL "https://download.docker.com/linux/${OS_ID}/gpg" -o /etc/apt/keyrings/docker.asc
-    chmod a+r /etc/apt/keyrings/docker.asc
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/${OS_ID} \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update && apt-get install -y \
-       git \
-       docker-ce \
-       docker-ce-cli \
-       containerd.io \
-       docker-buildx-plugin \
-       docker-compose-plugin
-    systemctl enable --now docker
-}
+# TODO: Test with "sh -s - server" if this does not work.
+curl -sfL https://get.k3s.io | sh -
 
-install_amzn() {
-    yum update && yum upgrade -y
-
-    # Install docker
-    yum install -y git docker
-    systemctl enable --now docker
-
-    # Install docker plugins
-    mkdir -p /usr/local/lib/docker/cli-plugins
-
-    curl -sL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) \
-      -o /usr/local/lib/docker/cli-plugins/docker-compose
-
-    # Set ownership to root and make executable
-    chown root:root /usr/local/lib/docker/cli-plugins/docker-compose
-    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-}
-
-OS_ID=$(awk -F= '$1=="ID" { print $2 }' /etc/os-release | tr -d '"')
-
-case $OS_ID in
-    "debian")
-        install_debian
-        usermod -aG docker admin
-        ;;
-    "ubuntu")
-        install_debian
-        usermod -aG docker ubuntu
-        ;;
-    "amzn")  # Amazon Linux
-        install_amzn
-        usermod -aG docker ec2-user
-        ;;
-    *)
-        echo "Unsupported distribution: $OS_ID"
-        exit 1
-        ;;
-esac
+k3s kubectl get node
