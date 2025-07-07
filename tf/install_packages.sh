d3f84da0886d977ba80d0338734f4af90c44a03e
Maciej Barć <xgqt@gentoo.org>
2025-07-08 00:17:33 +0200

    chore(tf/install_packages.sh): disable traefik
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- tf/install_packages.sh
+++ tf/install_packages.sh
@@ -7,7 +7,7 @@ export TERM="dumb"
 
 export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 export K3S_KUBECONFIG_MODE="644"
-export INSTALL_K3S_EXEC="--disable=metrics-server --node-name=aws --write-kubeconfig-mode=${K3S_KUBECONFIG_MODE}"
+export INSTALL_K3S_EXEC="--disable='metrics-server,traefik' --node-name=aws --write-kubeconfig-mode=${K3S_KUBECONFIG_MODE}"
 
 # Install K3S.
 curl -sfL https://get.k3s.io | sh -
