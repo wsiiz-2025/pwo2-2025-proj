f6e2b26bbe341b8dea8a482a482f7dcd706ed00e
Maciej Barć <xgqt@gentoo.org>
2025-07-07 23:04:23 +0200

    chore(tf/install_packages.sh): install helm
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- tf/install_packages.sh
+++ tf/install_packages.sh
@@ -7,7 +7,10 @@ export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 export K3S_KUBECONFIG_MODE="644"
 export INSTALL_K3S_EXEC="--disable=metrics-server --node-name=aws --write-kubeconfig-mode=${K3S_KUBECONFIG_MODE}"
 
-# TODO: Test with "sh -s - server" if this does not work.
+# Install K3S.
 curl -sfL https://get.k3s.io | sh -
 
 k3s kubectl get node
+
+# Install Helm.
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
